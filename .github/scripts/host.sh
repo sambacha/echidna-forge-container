8af5b5b75f39a17cf12e215f191360823dbdc954
sam bacha <sam@freighttrust.com>
2022-05-09 15:11:19 -0700

    host

---
+++ .github/scripts/host.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ -z ${PREFIX+x} ]; then
+  export PREFIX="$HOME/.local"
+fi
+
+if [ -z ${HOST_OS+x} ]; then
+  export HOST_OS=`uname`
+  if [ "$HOST_OS" = "Darwin" ]; then
+    export HOST_OS="macOS"
+  fi 
+fi
+
+if [ "$HOST_OS" = "Linux" ]; then
+  EXT=".a"
+elif [ "$HOST_OS" = "macOS" ]; then
+  EXT=".dylib"
+else
+  echo "unrecognized host"
+  exit 1
+fi
